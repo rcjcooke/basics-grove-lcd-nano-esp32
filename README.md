0b93aff88771f321903a7073625df780eb801049
Ray Cooke <rcjcooke@gmail.com>
2023-11-09 20:12:46 +0000

    First working cut

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # basics-grove-lcd-nano-esp32
-Grove LCD RGB Backlight V4.0 against a Arduino Nano ESP32
+Grove LCD RGB Backlight V4.0 running from an Arduino Nano ESP32 via a Level Shifter.
+
+![Demo Video](./media/demo.mp4)
+
+**NOTE**: Arduino Nano ESP32 (based on the Espressif microcontroller) is a 3.3V device, whereas V4 of the Grove LCD is a 5V device.
+
+The Grove LCD is controlled on the IC2 bus. The [IC2 protocol](https://www.robot-electronics.co.uk/i2c-tutorial) allows for [different voltage levels](https://www.electronics-tutorials.ws/connectivity/i2c-voltage-level-translators.html), however you can't mix voltages on the same device as the supply voltage will be used as a reference for whether the signal lines are high or low. Therefore it is necessary to shift the voltage level of the signal lines between devices that operate on different voltages. This is achieved with a "Level Shifter" (also know as a level converter, logic level shifter, or voltage level translator). This simple way to do this is with a MOSFET in a circuit as follows:
+
+![Level Shifter Circuit Diagram](./media/levelshiftercircuit.png)
+I used an off-the-shelf board that contains 4 of these circuits ([BOB-12009](https://www.sparkfun.com/products/12009)) from SparkFun in this example, though I only need two in this case for the SDA and SCL signals of the IC2 bus. I supplied the 5V from an external power supply and the Nano was powered from USB.
+
+## Rainbow backlight
+The Grove LCD RGB backlight can change backlight colour (that's kinda the point :) ). I wanted to do something interesting with it, so I decided to loop through all the colour combinations. I used the following diagram I [found](https://www.freestylersupport.com/wiki/tutorial:sequences_ideas:rainbow_tutorial) on the Googles to put together the algorithm you'll find in the source code.
+
+![Rainbow sequence diagram](./media/rainbowsequence.png)
+
---
+++ software/src/main.cpp
@@ -0,0 +1,82 @@
+/*
+
+WARNING: This is written to be deployed on an Arduino Nano ESP 32. That's a 3.3V
+device. The Grove LCD Display (at least V4 that I'm using - I understand V5
+might be 3.3V compatible) operates at 5V. This is therefore intended to work
+with a level shifter. See the README for circuit details. Without the level
+shifter you run the risk of damaging your microcontroller, depending on how you
+wire up your 5 V supply and LCD display with your Arduino.
+
+*/
+#include <Arduino.h>
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+/************************
+ * Variables
+ ************************/
+// The LCD display
+rgb_lcd lcd;
+
+/************************
+ * Entry point methods
+ ************************/
+void setup() {
+  // Start the IC2 interface
+  Wire.begin();
+  // Set up the LCD display (16 characters x 2 rows)
+  // Start up the display
+  lcd.noCursor();
+  lcd.noBlink();
+  lcd.begin(16, 2);
+  
+  // Turn backlight off
+  lcd.setRGB(0,0,0);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+  
+  delay(1000);
+
+}
+
+void loop() {
+  // Set the cursor to column 0, line 1
+  lcd.setCursor(0, 1);
+  // Print the number of seconds since reset
+  lcd.print(millis()/1000);
+  // Rainbow backlight
+  static unsigned char r = 255;
+  static unsigned char g = 0;
+  static unsigned char b = 0;
+  static int segment = 0;
+  switch (segment) {
+    case 0:
+      g = g + 5;
+      if (g == 255) segment++;
+      break;
+    case 1:
+      r = r - 5;
+      if (r == 0) segment++;
+      break;
+    case 2:
+      b = b + 5;
+      if (b == 255) segment++;
+      break;
+    case 3:
+      g = g - 5;
+      if (g == 0) segment++;
+      break;
+    case 4:
+      r = r + 5;
+      if (r == 255) segment++;
+      break;
+    case 5:
+      b = b - 5;
+      if (b == 0) segment = 0;
+      break;
+    default:
+      break;
+  }
+  lcd.setRGB(r,g,b);
+  delay(100);
+}
